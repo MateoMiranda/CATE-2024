a66c739f9cb841f83a451c1a46aadd7bf831b975
MateoMiranda <83238528+MateoMiranda@users.noreply.github.com>
2024-10-01 22:09:01 -0300

    Add files via upload

---
+++ Arreglo10/src/principal/main.java
@@ -0,0 +1,27 @@
+package principal;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese una cadena de texto: ");
+        String cadena = scanner.nextLine();
+
+        int numVocales = contarVocales(cadena);
+
+        System.out.println("El número de vocales en la cadena es: " + numVocales);
+    }
+
+    public static int contarVocales(String cadena) {
+        int contador = 0;
+        String vocales = "aeiouAEIOU";
+        for (char c : cadena.toCharArray()) {
+            if (vocales.indexOf(c) != -1) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+}
